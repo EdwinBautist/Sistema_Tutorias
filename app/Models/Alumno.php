edba0fb0e6a95c8315652213594d5907aa78f674
Edwin Bautista Zintzun <2105732g@umich.mx>
2026-08-10 22:17:18 -0600

    Creación de controlador y modelos: Alumno y Profesor

---
+++ app/Models/Alumno.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Alumno extends Model
+{
+    protected $table = 'Alumno';
+    protected $primaryKey = 'matricula';
+    public $keyType = 'string';
+    public $timestamps = false;
+
+    protected $fillable = ['matricula', 'nombre', 'apell_paterno', 'apell_materno','correo','estatus','semestre','carrera','token_qr'];
+
+    //Tal vez algún método para normalizar los nombres (quitar acentos)
+}
---
+++ app/Models/Profesor.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profesor extends Model
+{
+    protected $table = 'Profesor';
+    protected $primaryKey = 'num_empleado';
+    public $keyType = 'string';
+    public $timestamps = false;
+
+    protected $fillable = ['num_empleado', 'nombre', 'apell_paterno', 'apell_materno', 'correo', 'tutor'];
+
+    //Tal vez algún método para normalizar los nombres (quitar acentos)
+
+    
+}
